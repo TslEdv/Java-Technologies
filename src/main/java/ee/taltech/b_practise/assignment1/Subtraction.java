a6f7925cbc1fe04da137f0d01ff545f7731fb082
Edvin Ess <edvess@taltech.ee>
2021-02-16 21:54:19 +0200

    Lamborghini and formatting

--- src/main/java/ee/taltech/b_practise/assignment1/Subtraction.java
+++ src/main/java/ee/taltech/b_practise/assignment1/Subtraction.java
@@ -5,15 +5,16 @@ public class Subtraction {
     //todo A create a static function that takes in two ints
     // and returns the result of subtracting second element from the first
     // p.s pay attention to function and parameter names
-    public static int substract (int x1, int x2){
-        return x1-x2;
+    public static int substract(int x1, int x2) {
+        return x1 - x2;
     }
+
     public static void main(String[] args) {
         //todo B
         // - call your newly created function with 5 and 2, print out the result
         // - call your newly created function with -5 and -2, print out the result
-        System.out.println(substract(5,2));
-        System.out.println(substract(-5,-2));
+        System.out.println(substract(5, 2));
+        System.out.println(substract(-5, -2));
         //todo C if you were to add another method
         // with the same name, but 3 inputs, then it would be an example of?
         // a) overreaching?
--- src/main/java/ee/taltech/b_practise/assignment2/Lamborghini.java
+++ src/main/java/ee/taltech/b_practise/assignment2/Lamborghini.java
@@ -6,9 +6,29 @@ public class Lamborghini {
     //todo B add a constructor which initializes both of the fields
     //todo C add 2 instance methods to this class (other than getters or setters)
 
+    private int numberofcars;
+    private String color;
+
+    public Lamborghini(int numberofcars, String color) {
+        this.numberofcars = numberofcars;
+        this.color = color;
+    }
+
+    private int BuyNewCar(int numberofcars) {
+        return (numberofcars + 1);
+    }
+
+    private String PaintCar(String color) {
+        return color = "pink";
+    }
+
     public static void main(String[] args) {
         //todo D create a new instance of your class
-
+        Lamborghini lambo = new Lamborghini(1, "black");
         //todo E call 2 of your newly created methods
+        lambo.color = lambo.PaintCar(lambo.color);
+        lambo.numberofcars = lambo.BuyNewCar(lambo.numberofcars);
+        System.out.println(lambo.color);
+        System.out.println(lambo.numberofcars);
     }
 }
