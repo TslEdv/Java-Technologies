d1bbc5643e1481962a88207d854f49649eb3c3ae
Kristiina Šamanina <krsama@ttu.ee>
2021-02-21 23:02:13 +0200

    theory filled

--- src/main/java/ee/taltech/a_theory/question2/Functions.java
+++ src/main/java/ee/taltech/a_theory/question2/Functions.java
@@ -3,13 +3,13 @@ package ee.taltech.a_theory.question2;
 public class Functions {
 
     // todo check all that are TRUE [x] (letters A-H are for grading)
-    //  A [] function is a piece of code that takes in variables and returns an output
-    //  B [] in java functions are different from methods as functions can be created separately while methods are connected to an object
-    //  C [] method names are usually written as verbs
-    //  D [] static methods means that the method returns no value
-    //  E [] static void methods can be called without creating a new instance of a class
-    //  F [] in real life static methods are used as utility functions
-    //  G [] if you want your method to be used by other classes in other packages you declare it as protected
-    //  H [] private method are accessible only inside a class
+    //  A [false] function is a piece of code that takes in variables and returns an output
+    //  B [x] in java functions are different from methods as functions can be created separately while methods are connected to an object
+    //  C [false] method names are usually written as verbs
+    //  D [false] static methods means that the method returns no value
+    //  E [x] static void methods can be called without creating a new instance of a class
+    //  F [x] in real life static methods are used as utility functions
+    //  G [x] if you want your method to be used by other classes in other packages you declare it as protected
+    //  H [x] private method are accessible only inside a class
 }
 
