7125b50629eb55c369ba63a8d01472add79fe6d1
Kristiina Šamanina <krsama@ttu.ee>
2021-02-21 23:15:34 +0200

    theory filled

--- src/main/java/ee/taltech/a_theory/question3/Compilation.java
+++ src/main/java/ee/taltech/a_theory/question3/Compilation.java
@@ -6,18 +6,19 @@ public class Compilation {
     // in your own words (do not use wiki definitions)
     // What is compilation?
     // Answer:
-    //
-    //
+    //It's taking the code that is written (source code),
+    // and converting them into bytecode so Java VM will know what to do.
     //
     //
     //
     //todo B
     // Name 2 benefits of compilation?
     // Benefits:
-    // 1
-    // 2
+    // 1 you can write code and compile it on JVM and run it anywhere, if anyone wants to acces it ,
+    // person needs to have JVM for that.
+    // 2 compiles easily on other operating systems, because Java bytecode runs anywhere where is its enviroment.
     //todo C
     // Name 1 disadvantage of compilation?
     // Disadvantage:
-    // 1
+    // 1 Its not for free and takes longer to compile.
 }
